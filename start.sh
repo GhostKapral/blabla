75c8e042dc6b5ec95ad593001a6e4aaabfcfd703
GhostKapral <148318307+GhostKapral@users.noreply.github.com>
2023-10-18 12:10:50 +0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/De9Max/sp-server-setter/master/main.py
+wget https://raw.githubusercontent.com/GhostKapral/blabla/main/main.py
 yum -y install python3
 python3 main.py
